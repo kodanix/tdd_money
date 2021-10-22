8e61406b7d49798923434a58f2376cb3c20aa423
kodanix <84291304+kodanix@users.noreply.github.com>
2021-10-22 22:24:25 +0200

    Refactoring the code
    deleting most of the code, while trying to maintain the logic.

--- src/greetings.js
+++ src/greetings.js
@@ -1,93 +1,76 @@
-function hello(a) {
-    megoldas = ""
-    if(a.length == 0){
-      return "Hello, my friend."
-    }else if(a.length == 1 && a[0]==a[0].toUpperCase()){
-      return "HELLO " + a[0] + "!"
-    }else if(a.length == 1){
-      return "Hello, " + a[0] + "."
-    }else if(a.length > 1){
-      hasupper = false
-      hascomma = false
-      b = []
-      uppercase = []
-      for(let i = 0; i < a.length; i++){
-        if(a[i] == a[i].toUpperCase()){
-          hasupper = true
-          uppercase.push(a[i]);
-          a.splice(i, 1)
-          if(a[i].includes(",")){
-            for(let k = 0; k < a[i].split(',').length; k++){
-                b.push(a[i]);
-            }
-          }else{
-              b.push(a[i]);
-          }
-        }else{
-            if(a[i].includes(",")){
-              hascomma = true
-                for(let k = 0; k < a[i].split(',').length; k++){
-                    b.push( a[i].split(',')[k]);
-                }
-              }else{
-                  b.push(a[i]);
-            }
-        }
-      }
-      if(b.length > 0){
-          a = b;
-      }
-      if(hascomma){
-        megoldas = "Hello, "
-        for(let i = 0; i < a.length; i++){
-          if(i==a.length-1){
-            megoldas += ", and" + a[i] + "."
-          }else{
-            if(i==a.length-2){
-              megoldas += a[i]
-            }else{
-              megoldas += a[i] + ", "
-            }
-          }
-        }
-        return megoldas
-      }
-      if(hasupper){
-        megoldas = "Hello, "
-        for(let i = 0; i < a.length; i++){
-          if(i==a.length-1){
-            megoldas += "and " + a[i] + ". "
-          }else{
-            megoldas += a[i] + ", "
-          }
-        }
-        megoldas += "AND HELLO "
-        for(let i = 0; i < uppercase.length; i++){
-          if(i==uppercase.length-1){
-            megoldas += "AND " + uppercase[i] + "!"
-          }else{
-            megoldas += uppercase[i] + ", "
-          }
-        }
-        return megoldas
-      }
-      if(!hasupper){
-        megoldas = "Hello, "
-        for(let i = 0; i < a.length; i++){
-          if(i==a.length-1){
-            megoldas += " and " + a[i] + "."
-          }else{
-            if(i==a.length-2){
-              megoldas += a[i]
-            }else{
-              megoldas += a[i] + ", "
-            }
-          }
-        }
-        return megoldas
-      }
-    }
-    return 0;
+function textMaker(param, isShouting){
+  let result = '';
+  let and = 'and ';
+  let hello = 'Hello, ';
+  let dot = '.';
+  if(isShouting){
+      and = 'AND ';
+      hello = 'HELLO ';
+      dot = '!';
   }
+
+  if(param.length === 1){
+      result = param[0];
+  }else if(param.length > 1){
+      let temp = '';
+      for(let i = 0; i < param.length; i++){
+          switch(i){
+              case(param.length-2) : {
+                  temp += (param[i] + ', ' + and);
+                  break;
+              }
+              case(param.length-1) : {
+                  temp += (param[i]);
+                  break;
+              }
+              default: {
+                  temp += (param[i] + ', ');
+                  break;
+              }
+          }
+      }
+      result += temp;
+ }
+
+  return hello + result + dot;
+}
+
+
+function hello(param) {
+  let lowercase = [];
+  let uppercase = [];
+  let temp = [];
+
+  //check for commas
+  for(let i = 0; i < param.length; i++){
+      if(param[i].includes(',')){
+          for(let k = 0; k < param[i].split(',').length; k++){
+              temp.push(param[i].split(',')[k].trim())
+          }
+      }else{
+          temp.push(param[i]);
+      }
+  }
+  param = temp;
+
+  //uppercase and lowercase names as arrays
+  for(let i = 0; i < param.length; i++){
+      if(param[i] === param[i].toUpperCase()){
+          uppercase.push(param[i]);
+      }else{
+          lowercase.push(param[i]);
+      }
+  }
+
+  if(lowercase.length === 0 && uppercase.length === 0){
+      return 'Hello, my friend.';
+  }else if(lowercase.length > 0 && uppercase.length === 0){
+      return textMaker(lowercase, false);
+  }else if(uppercase.length > 0 && lowercase.length === 0){
+      return textMaker(uppercase, true);
+  }else if(lowercase.length > 0 && uppercase.length > 0){
+      return  textMaker(lowercase, false) + " AND " + textMaker(uppercase, true);
+  }
+}
 
 module.exports = hello;
